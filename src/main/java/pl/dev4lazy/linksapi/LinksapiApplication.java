55dae0c5a65ada351c5c8215bc36ecc548132f51
tomaszwartak <tomasz.wartak@gmail.com>
2022-10-18 13:59:28 +0200

    Initial commit

---
+++ src/main/java/pl/dev4lazy/linksapi/LinksapiApplication.java
@@ -0,0 +1,13 @@
+package pl.dev4lazy.linksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinksapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dev4lazy/linksapi/LinksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dev4lazy.linksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
